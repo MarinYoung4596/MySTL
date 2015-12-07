909e2e873abf7e360547f92e49146ff0a6306624
MarinYoung <marinyoung4596@gmail.com>
2015-12-07 21:50:16 +0800

    update1207

--- MySTL/MySTL/alloc_impl.cpp
+++ MySTL/MySTL/alloc_impl.cpp
@@ -24,7 +24,7 @@ namespace MySTL
 			void *r = refill(ROUND_UP(bytes)); // refill free_list
 			return r;
 		}
-		else // if there's available node in free_list
+		else // if there's at least one node available in free_list
 		{
 			free_list[index] = list->next; // remove this block (list) from the free_list
 			return list;	// and return to construct object.
@@ -49,7 +49,7 @@ namespace MySTL
 			size_t index = FREE_LIST_INDEX(bytes);
 			// insert @q in the head of suitable node list in the @free_list,
 			// same with the insertion operation of single link list
-			obj *q = (obj *)p;
+			alloc::obj *q = (alloc::obj *)p;
 			q->next = free_list[index];
 			free_list[index] = q;
 		}
@@ -64,20 +64,88 @@ namespace MySTL
 	}
 
 
-	// 
+	// assume that @bytes is the multiple of 8.
 	void* alloc::refill(size_type bytes)
 	{
+		int nobjs = 20; // the number of nodes increased every time.
+		char* chunk = chunk_alloc(bytes, nobjs);
+		if (1 == nobjs) // only one nodes available, return this block
+			return chunk;
 
+		alloc::obj* volatile *list;
+		list = free_list + FREE_LIST_INDEX(bytes);
+		alloc::obj *result, *next, *curr;
+		result = (alloc::obj*)chunk;	// refill free_list in chunk
+
+		*list = next = (alloc::obj*)(chunk + bytes); // let @list point to the new configured memory
+		for (int i = 1; i < nobjs; ++i)	// loop start from 1, because the zeroth node would be returned to the user
+		{
+			curr = next;
+			next = (alloc::obj*)((char*)next + bytes);
+			curr->next = ((i == nobjs - 1) ? nullptr : next);
+		}
+		return result;
 	}
 
 
 	// allocate a space that contains @nOBJs blocks with the size of @bytes
 	//    @nOBJs might be reduced in different situations.
-	// @bytes: the bytes of a block
+	// @bytes: the bytes of a block£¬(assume that bytes is the multiple of 8)
 	// @nOBJs: number of blocks
 	char* alloc::chunk_alloc(size_type bytes, int &nOBJs)
 	{
+		char *result;
+		size_type required_bytes = bytes * nOBJs;		// the required bytes in total
+		size_type bytes_left = end_free - start_free;	// the left space in the memory pool
 
-	}
-}
+		if (bytes_left >= required_bytes)	// @bytes_left fulfills the requirement
+		{
+			result = start_free;
+			start_free += required_bytes;
+			return result;
+		}
+		else if (bytes_left >= bytes)	// @bytes_left can't entirely satisfy the requirement, but is sufficient for at least one blocks
+		{
+			nOBJs = bytes_left / bytes;
+			required_bytes = bytes * nOBJs;
+			result = start_free;
+			start_free += required_bytes;
+			return result;
+		}
+		else	// @bytes_left can't even provide free space for one block
+		{
+			size_type bytes_to_get = 2 * required_bytes + ROUND_UP(heap_size >> 4);
+			if (bytes_left > 0)
+			{
+				alloc::obj* volatile *list = free_list + FREE_LIST_INDEX(bytes_left);
+				((alloc::obj*)start_free)->next = *list;
+				*list = (alloc::obj*)start_free;
+			}
+
+			start_free = (char*)malloc(bytes_to_get);
+			if (nullptr == start_free)
+			{
+				alloc::obj* volatile *list, *p;
+				for (int i = bytes; i <= MAX_BYTES; i += ALIGN)
+				{
+					list = free_list + FREE_LIST_INDEX(i);
+					p = *list;
+					if (p != nullptr)
+					{
+						*list = p->next;
+						start_free = (char*)p;
+						end_free = start_free + i;
+						return chunk_alloc(bytes, nOBJs); // invoke itself recursively to correct @nOBJs
+					}
+				}
+				end_free = nullptr;
+				start_free = (char*)malloc(bytes_to_get);
+			}
+			heap_size += bytes_to_get;
+			end_free = start_free + bytes_to_get;
+			return chunk_alloc(bytes, nOBJs);  // invoke itself recursively to correct @nOBJs
+		}
+	} // end chunk_alloc
+
+} // end namespace
 
