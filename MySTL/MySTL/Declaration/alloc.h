afff5c40d8495577e17036d40c86495d13249aa6
MarinYoung <marinyoung4596@gmail.com>
2015-12-09 12:29:08 +0800

    add alloc.h

---
+++ MySTL/MySTL/Declaration/alloc.h
@@ -0,0 +1,52 @@
+#ifndef INCLUDED_ALLOC_H
+#define INCLUDED_ALLOC_H
+
+namespace MySTL
+{
+    class alloc
+    {
+    private:
+        typedef size_t     size_type;
+
+		// block increasing step length
+        enum { ALIGN = 8 };
+		// upper bound of block
+        enum { MAX_BYTES = 128 };
+		// number of free lists
+        enum { N_FREE_LISTS = MAX_BYTES / ALIGN };
+	
+	public:
+		// definition of node in the  free lists
+        union obj
+        {
+            union obj *next;
+            char client;
+		};
+
+	private:
+		// round up @bytes to a multiple of @ALIGN
+        static size_t ROUND_UP(size_type bytes)
+        { return ((bytes + ALIGN - 1) & ~(ALIGN - 1)); }
+
+		// choose an appropriate node according to the value of @bytes
+        static size_t FREE_LIST_INDEX(size_type bytes)
+        { return ((bytes + ALIGN - 1) / ALIGN - 1); }
+
+        static void* refill(size_type bytes);
+        static char* chunk_alloc(size_type bytes, int &nOBJs);
+
+	private:
+		static char *start_free; // start position of memory pool
+		static char *end_free;	// end position of memory pool
+		static size_type heap_size;
+
+		static obj* free_list[N_FREE_LISTS]; /* obj* */
+        
+    public:
+        static void* allocate(size_type bytes);
+        static void deallocate(void *p, size_type bytes);
+        static void* reallocate(void *p, size_type old_size, size_type new_size);
+    };
+}
+
+#endif
